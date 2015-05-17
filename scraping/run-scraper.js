f79f11683f8b249fa44159e47bf224fc41030568
Glen Maddern <glenmaddern@gmail.com>
2015-05-17 15:58:29 +1000

    slightly nicer output during

--- scraping/run-scraper.js
+++ scraping/run-scraper.js
@@ -3,22 +3,18 @@ var portfinder = require('portfinder'),
   exec = require('fluent-exec'),
   paths = require('./paths')
 
-console.log("looking for a port")
 portfinder.basePort = 1984
 
 portfinder.getPort(function (err, port) {
   if (err) throw err;
-  console.log("got here tho")
+  console.log("Port " + port + " is open.")
 
   pushserve({port: port, indexPath: 'src/200.html', path: 'src'}, function () {
-    console.log("Loaded up SRC on http://localhost:" + port)
-
     var cmd = 'phantomjs ' + __dirname +
       '/scrape.js http://localhost:' + port +
       ' "' + paths.join(' ') + '"'
-    console.log(cmd)
+    console.log('Running: ' + cmd)
     exec(cmd).then(function (output) {
-      console.log("ok!")
       console.log(output)
       process.exit()
     })
