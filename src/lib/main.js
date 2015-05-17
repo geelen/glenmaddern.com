576082783568f86a0323c3ffec0a0c31c2c6f148
Glen Maddern <glenmaddern@gmail.com>
2015-05-17 12:58:15 +1000

    chrome is giving me ok error messages, dont need this

--- src/lib/main.js
+++ src/lib/main.js
@@ -2,18 +2,13 @@ import React from 'react'
 import Router from 'react-router'
 import routes from './routes'
 
-export default new Promise((resolve, reject) => {
-  let mainElem = document.querySelector('main')
-  Router.run(routes, Router.HistoryLocation, (Root) => {
-    if (navigator.userAgent.match(/phantomjs/i)) {
-      mainElem.innerHTML = React.renderToString(React.createElement(Root))
-    } else {
-      React.render(React.createElement(Root), mainElem)
-    }
-    resolve()
-  })
-}).then(_ => {
-    console.log("Main rendered in " + Math.round(performance.now()) / 1000 + "s")
-  }, err => {
-    console.error(err)
-  })
+let mainElem = document.querySelector('main')
+Router.run(routes, Router.HistoryLocation, (Root) => {
+  if (navigator.userAgent.match(/phantomjs/i)) {
+    mainElem.innerHTML = React.renderToString(React.createElement(Root))
+  } else {
+    React.render(React.createElement(Root), mainElem)
+  }
+})
+
+console.log("Main rendered in " + Math.round(performance.now()) / 1000 + "s")
