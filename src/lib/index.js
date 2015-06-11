2b9b1363c5ea31a815657d23b22d22b89f35ba3d
Glen Maddern <glenmaddern@gmail.com>
2015-06-11 11:19:31 +1000

    commenting out everything to just get basic live-reload to work

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Router from 'react-router'
-import routes from './routes'
-import '../styles/index'
-
-let mainElem = document.querySelector('main')
-Router.run(routes, Router.HistoryLocation, (Root) => {
-  if (navigator.userAgent.match(/phantomjs/i)) {
-    mainElem.innerHTML = React.renderToString(React.createElement(Root))
-  } else {
-    React.render(React.createElement(Root), mainElem)
-  }
-})
+//import routes from './routes'
+//import '../styles/index'
+//
+//let mainElem = document.querySelector('main')
+//Router.run(routes, Router.HistoryLocation, (Root) => {
+//  if (navigator.userAgent.match(/phantomjs/i)) {
+//    mainElem.innerHTML = React.renderToString(React.createElement(Root))
+//  } else {
+//    React.render(React.createElement(Root), mainElem)
+//  }
+//})
 
 console.log("Main rendered in " + Math.round(performance.now()) / 1000 + "s")
+export let __hotReload = true
