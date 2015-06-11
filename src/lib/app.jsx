ad3ce92541fc4bd277fd6eab227d11031b789230
Glen Maddern <glenmaddern@gmail.com>
2015-06-11 11:32:47 +1000

    live-reloading works even if the event propagates all the way to the top!

--- src/lib/app.jsx
+++ src/lib/app.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Router from 'react-router'
 let {RouteHandler} = Router
 
-import Nav from './nav.jsx!'
-import Footer from './footer.jsx!'
-import styles from './app.metacss'
+//import Nav from './nav.jsx!'
+//import Footer from './footer.jsx!'
+//import styles from './app.metacss'
 
 export default class App extends React.Component {
   render() {
-    return <div className={styles.body}>
-      <Nav />
-      <RouteHandler />
-      <Footer />
-    </div>
+    return <h1>HIYO</h1>
+    //return <div className={styles.body}>
+    //  <Nav />
+    //  <RouteHandler />
+    //  <Footer />
+    //</div>
   }
 }
+export let __hotReload = true
