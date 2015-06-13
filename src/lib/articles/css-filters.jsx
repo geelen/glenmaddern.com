fbd8cf0ba9b1d8b1c781d882402e930fc075f411
Glen Maddern <glenmaddern@gmail.com>
2015-06-13 21:09:07 +1000

    with yaml front matter, get rid of that extra file

--- src/lib/articles/css-filters.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Article from './article.jsx!'
-import body from './css-filters.md!markdown'
-
-let CssFilters = <Article
-  article={body}
-></Article>
-
-export default CssFilters
--- src/lib/articles/index.jsx
+++ src/lib/articles/index.jsx
@@ -1,11 +1,14 @@
+import React from 'react'
+import Article from './article.jsx!'
+
 import js2015 from './javascript-in-2015.jsx!'
 import amcss from './introducing-am-css.jsx!'
 import metaquery from './metaquery.jsx!'
-import cssfilters from './css-filters.jsx!'
+import cssfilters from './css-filters.md!markdown'
 
 export default {
   'javascript-in-2015': js2015,
   'introducing-am-css': amcss,
   'metaquery-and-the-end-of-media-queries': metaquery,
-  'css-filters-gifs-and-performance': cssfilters
+  'css-filters-gifs-and-performance': <Article article={cssfilters}></Article>
 }
