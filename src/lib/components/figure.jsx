134fdedd49759107bca8a0244a09e0f84a5ee331
Glen Maddern <glenmaddern@gmail.com>
2015-08-15 17:06:15 +1000

    this is getting good, but oh god its long

--- src/lib/components/figure.jsx
+++ src/lib/components/figure.jsx
@@ -4,7 +4,8 @@ import styles from './figure.css!'
 
 export default class Figure extends React.Component {
   render() {
-    return <figure className={styles.figure}>
+    let figClass = this.props.variant || 'figure'
+    return <figure className={styles[figClass]}>
       <img className={styles.img} src={this.props.src} alt={this.props.alt}/>
       <figcaption className={styles.figcaption}>{this.props.children}</figcaption>
     </figure>
